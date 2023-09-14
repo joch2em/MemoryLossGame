0ae3c1dac2d45a45b52604434d5e51b99c52de65
joch3em <57416031+joch2em@users.noreply.github.com>
2023-09-14 11:26:24 +0200

    github setup

---
+++ Assets/Scripts/GlowingEffect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingEffect : MonoBehaviour
+{
+
+    public Color32 GlowingColor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
